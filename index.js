582acd2af39fb4510e324919daa824e4213c7083
Amarjeet Sahoo <amarjeethappysahoo@gmail.com>
2020-10-27 21:29:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      "http://api.openweathermap.org/data/2.5/weather?q=Odisha&units=metric&appid=b14425a6554d189a2d7dc18a8e7d7263"
+    )
+      .on("data", (chunk) => {
+        const objdata = JSON.parse(chunk);
+        const arrData = [objdata];
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join("");
+        res.write(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  } else {
+    res.end("File not found");
+  }
+});
+
+server.listen(8000, "127.0.0.1");
